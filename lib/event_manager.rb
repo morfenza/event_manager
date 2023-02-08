976ac202730f0045cf2a6e0b015623bbca7419ce
Morfenza <ianlourenco@protonmail.com>
2023-02-08 19:48:24 -0300

    Do iteration 0

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,1 +1,10 @@
 puts 'Event Manager Initialized'
+
+lines = File.readlines('event_attendees.csv')
+lines.each_with_index do |line, idx|
+  next if idx.zero?
+
+  columns = line.split(',')
+  name = columns[2]
+  puts name
+end
