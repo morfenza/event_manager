45a4d9fb4c9bf651c78287c5c6dfd417ec9f6388
Morfenza <ianlourenco@protonmail.com>
2023-02-09 14:29:01 -0300

    Do iteration 3

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,10 +1,31 @@
 # frozen_string_literal: true
 
 require 'csv'
+require 'google/apis/civicinfo_v2'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
+
+# rubocop:disable Metrics/MethodLength
+def legislators_by_zipcode(zipcode)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    legislators = civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: %w[legislatorUpperBody legislatorLowerBody]
+    )
+    legislators = legislators.officials
+
+    legislators.map(&:name).join(', ')
+  rescue StandardError
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+# rubocop:enable Metrics/MethodLength
 
 puts 'Event Manager Initialized'
 
@@ -18,5 +39,7 @@ contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
 
-  puts "#{name} #{zipcode}"
+  legislators = legislators_by_zipcode(zipcode)
+
+  puts "#{name} #{zipcode} #{legislators}"
 end
