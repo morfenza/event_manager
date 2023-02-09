00f7043094f5831f845327d08a7c64e225773968
Morfenza <ianlourenco@protonmail.com>
2023-02-09 15:13:35 -0300

    Do iteration 4

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,6 +2,7 @@
 
 require 'csv'
 require 'google/apis/civicinfo_v2'
+require 'erb'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
@@ -13,21 +14,31 @@ def legislators_by_zipcode(zipcode)
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
   begin
-    legislators = civic_info.representative_info_by_address(
+    civic_info.representative_info_by_address(
       address: zipcode,
       levels: 'country',
       roles: %w[legislatorUpperBody legislatorLowerBody]
-    )
-    legislators = legislators.officials
-
-    legislators.map(&:name).join(', ')
+    ).officials
   rescue StandardError
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
 end
 # rubocop:enable Metrics/MethodLength
 
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
 puts 'Event Manager Initialized'
+
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new(template_letter)
 
 contents = CSV.open(
   'event_attendees.csv',
@@ -36,10 +47,12 @@ contents = CSV.open(
 )
 
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
-  zipcode = clean_zipcode(row[:zipcode])
 
+  zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
 
-  puts "#{name} #{zipcode} #{legislators}"
+  form_letter = erb_template.result(binding)
+  save_thank_you_letter(id, form_letter)
 end
